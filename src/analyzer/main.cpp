0c594408785784da7d1e6fb2ffb01a55136d7c5e
momo5502 <mauriceheumann@gmail.com>
2025-03-22 10:09:15 +0100

    Performance optimization

--- src/analyzer/main.cpp
+++ src/analyzer/main.cpp
@@ -156,7 +156,7 @@ namespace
 
             auto read_handler = [&, section, concise_logging](const uint64_t address, size_t, uint64_t) {
                 const auto rip = win_emu.emu().read_instruction_pointer();
-                if (win_emu.mod_manager.find_by_address(rip) != win_emu.mod_manager.executable)
+                if (!win_emu.mod_manager.executable->is_within(rip))
                 {
                     return;
                 }
@@ -178,7 +178,7 @@ namespace
 
             const auto write_handler = [&, section, concise_logging](const uint64_t address, size_t, uint64_t) {
                 const auto rip = win_emu.emu().read_instruction_pointer();
-                if (win_emu.mod_manager.find_by_address(rip) != win_emu.mod_manager.executable)
+                if (!win_emu.mod_manager.executable->is_within(rip))
                 {
                     return;
                 }
