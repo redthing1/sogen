0aae376a511c0b7f979106fa3f29196fd3d193c5
momo5502 <mauriceheumann@gmail.com>
2025-04-21 17:54:40 +0200

    Add new site

---
+++ page/src/components/settings-menu.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Checkbox } from "./ui/checkbox";
+import { Label } from "./ui/label";
+
+import { Settings } from "@/settings";
+
+interface SettingsMenuProps {
+    settings: Settings;
+    onChange: (settings: Settings) => void;
+ };
+
+export class SettingsMenu extends React.Component<SettingsMenuProps, Settings> {
+
+    constructor(props: SettingsMenuProps) {
+        super(props);
+        this.getSettings = this.getSettings.bind(this);
+        this.state = props.settings;
+    }
+
+    getSettings() {
+        return this.state;
+    }
+
+    updateSettings(settings: Settings) {
+        this.setState(() => settings);
+    }
+
+    componentDidUpdate() {
+        this.props.onChange(this.state);
+    }
+
+    render() {
+        return (
+            <div className="grid gap-4">
+                <div className="space-y-2">
+                    <h4 className="font-medium leading-none">Settings</h4>
+                    <p className="text-sm text-muted-foreground">
+                        Set the settings for the emulation.
+                    </p>
+                </div>
+
+                <div className="flex gap-6">
+                    <Checkbox id="settings-verbose" checked={this.state.verbose} onCheckedChange={(checked: boolean) => {
+                        this.setState({ verbose: checked });
+                    }} />
+                    <Label htmlFor="settings-verbose">Verbose Logging</Label>
+                </div>
+
+                <div className="flex gap-6">
+                    <Checkbox id="settings-concise" checked={this.state.concise} onCheckedChange={(checked: boolean) => {
+                        this.setState({ concise: checked });
+                    }} />
+                    <Label htmlFor="settings-concise">Concise Logging</Label>
+                </div>
+
+                <div className="flex gap-6">
+                    <Checkbox id="settings-silent" checked={this.state.silent} onCheckedChange={(checked: boolean) => {
+                        this.setState({ silent: checked });
+                    }} />
+                    <Label htmlFor="settings-silent">Silent Logging</Label>
+                </div>
+
+                <div className="flex gap-6">
+                    <Checkbox id="settings-buffer" checked={this.state.bufferStdout} onCheckedChange={(checked: boolean) => {
+                        this.setState({ bufferStdout: checked });
+                    }} />
+                    <Label htmlFor="settings-buffer">Buffer stdout</Label>
+                </div>
+            </div>
+        );
+    }
+};
---
+++ page/src/components/status-indicator.tsx
@@ -0,0 +1,21 @@
+import { Badge } from '@/components/ui/badge'
+import { CircleIcon } from '@radix-ui/react-icons';
+
+export interface StatusIndicatorProps {
+    running: boolean;
+};
+
+export function StatusIndicator(props: StatusIndicatorProps) {
+
+    const getText = () => {
+        return props.running ? " Running" : " Stopped";
+    };
+
+    const getColor = () => {
+        return props.running ? "bg-lime-600" : "bg-orange-600";
+    }
+
+    return (
+        <Badge variant="outline"><CircleIcon className={getColor() + " rounded-full mr-1 n  duration-200 ease-in-out"} color='transparent' />{getText()}</Badge>
+    );
+}
