ac45f4fee491e58e2edef801807323a56a48bf76
momo5502 <mauriceheumann@gmail.com>
2025-04-22 15:51:27 +0200

    Redirect back on invalid routes

--- page/src/App.tsx
+++ page/src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Playground } from "./Playground";
 import { LandingPage } from "./LandingPage";
 
@@ -7,14 +7,15 @@ import "./App.css";
 
 function App() {
   return (
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <HashRouter>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/playground" element={<Playground />} />
-          </Routes>
-        </HashRouter>
-      </ThemeProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <HashRouter>
+        <Routes>
+          <Route path="*" element={<Navigate to="/" replace />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/playground" element={<Playground />} />
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
