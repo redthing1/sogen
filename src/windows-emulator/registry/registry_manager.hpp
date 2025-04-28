05a965fe54a031aeb6d9f64679684854ec648c61
Igor Pissolati <igo08an@hotmail.com>
2025-04-28 12:54:55 -0300

    Fix non-windows builds

--- src/windows-emulator/registry/registry_manager.hpp
+++ src/windows-emulator/registry/registry_manager.hpp
@@ -5,6 +5,25 @@
 #include "serialization_helper.hpp"
 #include "../handles.hpp"
 
+#ifndef OS_WINDOWS
+#define REG_NONE (0ul) // No value type
+#define REG_SZ   (1ul) // Unicode nul terminated string
+#define REG_EXPAND_SZ \
+    (2ul)                                    // Unicode nul terminated string
+                                             // (with environment variable references)
+#define REG_BINARY                     (3ul) // Free form binary
+#define REG_DWORD                      (4ul) // 32-bit number
+#define REG_DWORD_LITTLE_ENDIAN        (4ul) // 32-bit number (same as REG_DWORD)
+#define REG_DWORD_BIG_ENDIAN           (5ul) // 32-bit number
+#define REG_LINK                       (6ul) // Symbolic Link (unicode)
+#define REG_MULTI_SZ                   (7ul) // Multiple Unicode strings
+#define REG_RESOURCE_LIST              (8ul) // Resource list in the resource map
+#define REG_FULL_RESOURCE_DESCRIPTOR   (9ul) // Resource list in the hardware description
+#define REG_RESOURCE_REQUIREMENTS_LIST (10ul)
+#define REG_QWORD                      (11ul) // 64-bit number
+#define REG_QWORD_LITTLE_ENDIAN        (11ul) // 64-bit number (same as REG_QWORD)
+#endif
+
 struct registry_key : ref_counted_object
 {
     utils::path_key hive{};
