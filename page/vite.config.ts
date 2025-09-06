517270e2cf7bc8b468fd69b886427f400df41e36
momo5502 <mauriceheumann@gmail.com>
2025-09-06 19:40:40 +0200

    Add PWA support

--- page/vite.config.ts
+++ page/vite.config.ts
@@ -1,11 +1,26 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
+
+const mb = 1024 ** 2;
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#0279E8",
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 100 * mb,
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
