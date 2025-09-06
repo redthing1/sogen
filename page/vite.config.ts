9be8e2700169d89097563d0505b3e3e73b6c48e5
momo5502 <mauriceheumann@gmail.com>
2025-09-06 20:40:44 +0200

    Cache resources

--- page/vite.config.ts
+++ page/vite.config.ts
@@ -3,8 +3,29 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
+import { RuntimeCaching } from "workbox-build";
 
 const mb = 1024 ** 2;
+
+function generateExternalCache(
+  pattern: string | RegExp,
+  name: string,
+): RuntimeCaching {
+  return {
+    urlPattern: pattern,
+    handler: "CacheFirst",
+    options: {
+      cacheName: name,
+      expiration: {
+        maxEntries: 10,
+        maxAgeSeconds: 60 * 60 * 24 * 365, // <== 365 days
+      },
+      cacheableResponse: {
+        statuses: [0, 200],
+      },
+    },
+  };
+}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -19,6 +40,17 @@ export default defineConfig({
       },
       workbox: {
         maximumFileSizeToCacheInBytes: 100 * mb,
+        globPatterns: ["**/*.{js,css,html,woff,woff2}"],
+        runtimeCaching: [
+          generateExternalCache(
+            /^https:\/\/momo5502\.com\/.*/i,
+            "momo5502-cache",
+          ),
+          generateExternalCache(
+            /^https:\/\/img\.youtube\.com\/.*/i,
+            "youtube-img-cache",
+          ),
+        ],
       },
     }),
   ],
