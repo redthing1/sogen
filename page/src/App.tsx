9a9e38d4a8ad6e474602851a40b80a44b06582c8
momo5502 <mauriceheumann@gmail.com>
2025-09-25 19:58:07 +0200

    Show loader when page is updating

--- page/src/App.tsx
+++ page/src/App.tsx
@@ -3,17 +3,50 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import { Playground, storeEmulateData } from "./playground";
 import { LandingPage } from "./landing-page";
-
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import "@fontsource/inter/latin.css";
 
 import "./App.css";
+import "./animation.css";
 
 function EmulateFile() {
   const { encodedData } = useParams();
   storeEmulateData(encodedData);
   return <Navigate to="/playground" replace />;
+}
+
+function isWindowLoading() {
+  return !!(window as any).loading;
+}
+
+function useLoader() {
+  const [isLoading, setIsLoading] = useState(isWindowLoading());
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setIsLoading(isWindowLoading());
+    }, 60);
+
+    return () => {
+      clearInterval(id);
+    };
+  });
+
+  return isLoading;
+}
+
+function Loader() {
+  const loading = useLoader();
+
+  if (!loading) {
+    return <></>;
+  }
+
+  return (
+    <div className="fixed z-9999 top-10 right-10 p-8 rounded-2xl ring-of-dots bg-[#00000081]"></div>
+  );
 }
 
 function App() {
@@ -28,6 +61,7 @@ function App() {
             <Route path="/emulate/:encodedData?" element={<EmulateFile />} />
           </Routes>
         </HashRouter>
+        <Loader />
       </TooltipProvider>
     </ThemeProvider>
   );
--- page/src/main.tsx
+++ page/src/main.tsx
@@ -4,15 +4,24 @@ import "./index.css";
 import App from "./App.tsx";
 import { registerSW } from "virtual:pwa-register";
 
+(window as any).loading = false;
+
+registerSW({
+  onNeedRefresh() {
+    setTimeout(() => {
+      window.location.reload();
+    }, 5000);
+  },
+  onOfflineReady() {},
+  onRegisteredSW(_, registration) {
+    registration?.addEventListener("updatefound", () => {
+      (window as any).loading = true;
+    });
+  },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
-
-registerSW({
-  onNeedRefresh() {
-    window.location.reload();
-  },
-  onOfflineReady() {},
-});
