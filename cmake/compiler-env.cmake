940fef48f06d72d52dec444ebc536cb0c417b978
ssvine <79405160+ssvine@users.noreply.github.com>
2026-02-05 13:37:51 +0300

    Optimize msvc release builds

--- cmake/compiler-env.cmake
+++ cmake/compiler-env.cmake
@@ -181,6 +181,17 @@ if(MSVC)
     /Zc:__cplusplus
   )
 
+  momo_add_c_and_cxx_release_compile_options(
+    /Gw
+    /GL
+  )
+
+  momo_add_release_link_options(
+    /OPT:REF
+    /OPT:ICF
+    /LTCG
+  )
+
   add_link_options(
     /INCREMENTAL:NO
   )
