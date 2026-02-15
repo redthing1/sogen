9322570980bcc076be1619d8ffe5d1fe2880ad20
ssvine <79405160+ssvine@users.noreply.github.com>
2026-02-16 02:21:57 +0300

    Add callback_list to support registering multiple callbacks

--- src/common/utils/function.hpp
+++ src/common/utils/function.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <functional>
+#include <vector>
+#include <algorithm>
+#include <cstddef>
 
 namespace utils
 {
@@ -52,5 +55,46 @@ namespace utils
         {
             return static_cast<bool>(func);
         }
+    };
+
+    using callback_id_type = std::size_t;
+
+    template <typename Signature>
+    class callback_list;
+
+    template <typename R, typename... Args>
+    class callback_list<R(Args...)>
+    {
+      public:
+        using function_type = std::function<R(Args...)>;
+
+        callback_id_type add(function_type fn)
+        {
+            callbacks.emplace_back(next_id, std::move(fn));
+            return next_id++;
+        }
+
+        void remove(callback_id_type id)
+        {
+            callbacks.erase(std::remove_if(callbacks.begin(), callbacks.end(), [id](auto& pair) { return pair.first == id; }),
+                            callbacks.end());
+        }
+
+        void operator()(Args... args) const
+        {
+            for (auto& [id, fn] : callbacks)
+            {
+                fn(args...);
+            }
+        }
+
+        explicit operator bool() const noexcept
+        {
+            return !callbacks.empty();
+        }
+
+      private:
+        std::vector<std::pair<callback_id_type, function_type>> callbacks;
+        callback_id_type next_id = 1;
     };
 }
